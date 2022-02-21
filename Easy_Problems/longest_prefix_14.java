e709b33f222d2c3666028a92ce52996232821667
Ayush <ayush8402@gmail.com>
2022-02-21 16:13:47 +0530

    longest prefix

---
+++ Easy_Problems/longest_prefix_14.java
@@ -0,0 +1,35 @@
+package Easy_Problems;
+
+public class longest_prefix_14 {
+    public String longestCommonPrefix(String[] strs) {
+        String min = strs[0];
+        for(String s:strs){
+            if(s.length()<min.length())
+                min = s;
+        }
+        boolean contains = false;
+        while(!contains){
+            contains=true;
+            for(int i=0; i<strs.length;i++){
+                if(!strs[i].substring(0,min.length()).equals(min)){
+                    System.out.println(strs[i].substring(0, min.length())+" "+min);
+                    contains= false;
+                }}
+                if(!contains){
+                    if(min.length()==1){
+                        return "";
+                    }
+                    min = min.substring(0, min.length() - 1);
+                    continue;
+                }
+                else{
+                    return min;
+                }
+        }
+        return min;
+    }
+    public static void main(String[] args) {
+        longest_prefix_14 o = new longest_prefix_14();
+        System.out.println(o.longestCommonPrefix(new String[]{"flower","flow","flight"}));
+    }
+}
