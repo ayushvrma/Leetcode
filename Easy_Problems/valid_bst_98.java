b909ebe3fa44d8254f5a130222c2bdb627c70332
Ayush <ayush8402@gmail.com>
2022-02-23 14:46:23 +0530

    minus of Double.max value

---
+++ Easy_Problems/valid_bst_98.java
@@ -0,0 +1,20 @@
+package Easy_Problems;
+import java.util.*;
+
+public class valid_bst_98 {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        double left_value = -Double.MIN_VALUE;
+        while(!s.isEmpty() || root !=null){
+            while(root!=null){
+                s.push(root);
+                root = root.left;
+            }
+            root = s.pop();
+            if(root.val<=left_value) return false;
+            left_value = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
