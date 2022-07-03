076aaafd8638be0df8915a99e89239dd85017e0d
Ayush <ayush8402@gmail.com>
2022-07-04 03:40:55 +0530

    sqrt 69

--- Easy_Problems/sqrtx_69.java
+++ Easy_Problems/sqrtx_69.java
@@ -2,14 +2,24 @@ package Easy_Problems;
 
 public class sqrtx_69 {
     public int mySqrt(int x) {
-        int i=1;
-        while(x/i*i!=0){
-            i++;
-            System.out.println(i);
+        int left = 1;
+        int right = x/2;
+        int mid;
+        while(left<=right){
+            mid = (left+right)/2;
+            if(mid*mid<x){
+                left = mid+1; //means we guessed way too less so reduce the lower limit
+                continue;
+            }
+            if(mid*mid>x){
+                right = mid-1; //means we guessed way too high so reduce the upper limit
+                continue;
+            }
+            return mid;
         }
-        return i-1;
+        return x; //for conditions x<=1, 
     }
     public static void main(String[] args) {
-        System.out.println(new sqrtx_69().mySqrt(2147483647));
+        System.out.println(new sqrtx_69().mySqrt(8));
     }
 }
