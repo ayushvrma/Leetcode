8f8ba0f846111fdd660e8001e3248d84bbaec3f9
Ayush <ayush8402@gmail.com>
2022-07-04 02:37:49 +0530

    67

--- Easy_Problems/add_binary_67.java
+++ Easy_Problems/add_binary_67.java
@@ -1,17 +1,26 @@
 package Easy_Problems;
 
 public class add_binary_67 {
-    public String addBinary(String a, String b) {
-        String s="";
-        int i=a.length()-1; int j=b.length()-1;
-        while(i>=0 || j>=0){
-            if(i>=0 && j>=0){
-                if(a.charAt(i)==0 && b.charAt(i)==0)
-                    s.concat("0");
-                else if((a.charAt(i)==0 && b.charAt(i)==1)||(a.charAt(i)==1 && b.charAt(i)==0))
-                    s.concat("1");
-                else if(a.charAt(i)==0 && b.charAt(i)==0)
-            }
+    public static String addBinary(String a, String b) {
+        String result = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int sum = 0;
+        while (i >= 0 || j >= 0) // this is for unequal strings
+        {
+            if(i>=0)sum += Integer.parseInt(String.valueOf(a.charAt(i)));
+            if(j>=0)sum += Integer.parseInt(String.valueOf(b.charAt(j)));
+            result = result.concat(Integer.toString(sum%2));
+            sum = sum / 2;
+            i--;
+            j--;
         }
+        if (sum !=0) {
+            result = result.concat("1");
+        }
+        return new StringBuilder(result).reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(addBinary("111", "1"));
     }
 }
