f8bc753f596addd88b1ce8c148e6cbddb3e8a5db
Ayush <ayush8402@gmail.com>
2022-07-04 14:02:57 +0530

    2 without string

--- Easy_Problems/pallindrome_9.java
+++ Easy_Problems/pallindrome_9.java
@@ -2,11 +2,26 @@ package Easy_Problems;
 
 public class pallindrome_9 {
     public boolean isPalindrome(int x) {
-        String num = Integer.toString(x);
-        for(int i=0; i<num.length();i++){
-            if(num.charAt(i)!=num.charAt(num.length()-1-i))
+        if(x<0)
+            return false;
+        int len = 0;
+        int temp = x;
+        int rev = 0;
+        while(temp!=0){
+            rev = rev*10 + temp%10;
+            temp/=10;
+            ++len;
+        }
+        while(rev!=0){
+            if(rev%10!=x%10)
                 return false;
+            rev/=10;
+            x/=10;
         }
         return true;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(new pallindrome_9().isPalindrome(121));
     }
 }
