949d605f53e97233e5ad4e80264ba7f5f5646854
Ayush <ayush8402@gmail.com>
2022-07-06 22:04:34 +0530

    160

--- Easy_Problems/intersection_LL_160.java
+++ Easy_Problems/intersection_LL_160.java
@@ -3,6 +3,22 @@ package Easy_Problems;
 public class intersection_LL_160 {
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null)
+            return null;
+        ListNode a = headA;
+        ListNode b = headB;
 
+        while(a!=b){
+            if(a==null)
+                a = headB;
+            else
+                a= a.next;
+
+            if(b==null)
+                b = headA;
+            else 
+                b = b.next;
+        }
+        return a;
     }
 }
