45aae3713ef514396ca429bd677cdee03c4c36d9
Ayush <ayush8402@gmail.com>
2022-07-08 16:47:50 +0530

    118

---
+++ Easy_Problems/pascal_118.java
@@ -0,0 +1,26 @@
+package Easy_Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pascal_118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> t = new ArrayList<>();
+        if(numRows==0) return t;
+        List<Integer> r1 = new ArrayList<>();
+        r1.add(1);
+        t.add(r1);
+        for(int i=1; i<numRows;i++){
+            List<Integer> prev_row = t.get(i-1);
+            List<Integer> curr_row = new ArrayList<>();
+            curr_row.add(1);
+            for(int j=1; j<i;j++) // starting from the 2nd elements, going to i cause no. of elements = row number
+            {
+                curr_row.add(prev_row.get(j-1)+prev_row.get(j));
+            }
+            curr_row.add(1);
+            t.add(curr_row);
+        }
+        return t;
+    }
+}
