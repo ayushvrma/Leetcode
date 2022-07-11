4ed677cf10393308f494e3d53db7e6ff0f331028
Ayush <ayush8402@gmail.com>
2022-07-11 14:27:38 +0530

    217

---
+++ Easy_Problems/duplicate_217.java
@@ -0,0 +1,14 @@
+package Easy_Problems;
+
+import java.util.HashSet;
+
+public class duplicate_217 {
+    public boolean containsDuplicate(int[] nums){
+        HashSet<Integer> h = new HashSet<>();
+        for(int i=0; i<nums.length;i++){
+            if(h.contains(nums[i])) return true;
+            h.add(nums[i]);
+        }
+        return false;
+    }
+}
